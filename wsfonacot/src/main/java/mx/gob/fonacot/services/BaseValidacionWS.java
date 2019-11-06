222ddd039a86bbbc7e09d771a9b01e555a661ec0
alphonsolaurent <alphonsolaurent@gmail.com>
2019-11-05 18:47:40 -0600

    Limpie el proyecto de codigo innecesario

--- wsfonacot/src/main/java/mx/gob/fonacot/services/BaseValidacionWS.java
+++ wsfonacot/src/main/java/mx/gob/fonacot/services/BaseValidacionWS.java
@@ -8,41 +8,42 @@ import javax.xml.ws.WebServiceContext;
 import javax.xml.ws.handler.MessageContext;
 
 public class BaseValidacionWS {
-	@Resource
+
+    @Resource
     WebServiceContext webServiceContext;
-	
-	boolean validaAcceso() {
-		MessageContext messageContext = webServiceContext.getMessageContext();
-		 
+
+    boolean validaAcceso() {
+        MessageContext messageContext = webServiceContext.getMessageContext();
+
         // get request headers
-        Map<?,?> requestHeaders = (Map<?,?>) messageContext.get(MessageContext.HTTP_REQUEST_HEADERS);
+        Map<?, ?> requestHeaders = (Map<?, ?>) messageContext.get(MessageContext.HTTP_REQUEST_HEADERS);
         List<?> usernameList = (List<?>) requestHeaders.get("user");
         List<?> passwordList = (List<?>) requestHeaders.get("password");
         List<?> entityList = (List<?>) requestHeaders.get("entity");
- 
+
         String username = "";
         String password = "";
         String entity = "";
- 
+
         if (usernameList != null) {
             username = usernameList.get(0).toString();
         }
- 
+
         if (passwordList != null) {
             password = passwordList.get(0).toString();
         }
         if (entityList != null) {
-        	entity = entityList.get(0).toString();
+            entity = entityList.get(0).toString();
         }
- 
+
         // of course this is not real validation
         // you should validate your users from stored databases credentials
         if (username.equals("usuarioqa") && password.equals("12121212") && entity.equals("INFONACOT")) {
- 
+
             return true;
- 
+
         } else {
-        	return false; 
-        } 
-	}
+            return false;
+        }
+    }
 }
